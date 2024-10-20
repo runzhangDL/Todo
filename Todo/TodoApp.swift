0fedd7e6209345aec3ae46ec45c2a305462405b8
runzhangDL <runzhang715@gmail.com>
2024-10-19 22:52:59 -0400

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 张润 on 2024/10/19.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
